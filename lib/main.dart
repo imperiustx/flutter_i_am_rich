98740b836e858678eeef04cffea447524bc4c7de
Hoang Vo <bktathoang@gmail.com>
2020-03-09 10:17:48 +0700

    change app icon

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,11 @@ void main() => runApp(
         home: Scaffold(
           backgroundColor: Colors.blueGrey,
           appBar: AppBar(
-            title: Text("Thanh Xuan"),
+            title: Text('I AM SO FUCKING RICH'),
             backgroundColor: Colors.blueGrey[900],
           ),
           body: Center(
-            child: Image(
-              image: NetworkImage('https://scontent.fsgn5-5.fna.fbcdn.net/v/t1.0-9/72714018_141057613964891_3096268973390430208_n.png?_nc_cat=108&_nc_sid=8024bb&_nc_ohc=WBl6kNnQg6AAX_jvwcY&_nc_ht=scontent.fsgn5-5.fna&oh=67c52c1a401b0d871af138efd5ba8c76&oe=5E80749B'),
-            ),
+            child: Image(image: AssetImage('images/diamond.png')),
           ),
         ),
       ),
